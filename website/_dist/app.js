f03b6c245bb3f7b869d0a0e4b4eb331064e8bd81
Dominic Dale <dominicdale@googlemail.com>
2023-08-06 14:35:28 +0100

    Overlay fixes

--- website/_dist/app.js
+++ website/_dist/app.js
@@ -163,26 +163,26 @@ const overlay_width = document.querySelector('.overlay__width')
 const overlay_height = document.querySelector('.overlay__height')
 const overlay_framed = document.querySelector('.overlay__framed')
 const overlay_contact = document.querySelector('.overlay__contact')
-const overlay_open = document.querySelector('.overlay__fullscreen')
+const overlay_open = document.querySelector('.overlay__download')
 const overlay_close = document.querySelector('.overlay__close')
 const contact_message = document.querySelector('#contactMessage')
 const overlay_fit = document.querySelector('.overlay__fit')
 
 function populateOverlay(e) {
     var img = e.getAttribute('data-img'),
-        img_crop = e.getAttribute('data-img-crop'),
         title = e.getAttribute('data-title'),
         price = e.getAttribute('data-price'),
         framed = e.getAttribute('data-framed'),
         description = e.getAttribute('data-description'),
-        imgWidth = e.getAttribute('data-imgWidth'),
-        imgHeight = e.getAttribute('data-imgHeight'),
         width = e.getAttribute('data-width'),
         height = e.getAttribute('data-height')
-    overlay_img.setAttribute('src', img_crop)
+    overlay_img.setAttribute('src', img)
     overlay_img.setAttribute('alt', title)
-    overlay_img.setAttribute('width', imgWidth)
-    overlay_img.setAttribute('height', imgHeight)
+    overlay_img.addEventListener('load', (event) => {
+        console.log('width', event.target.width)
+        console.log('height', event.target.height)
+        event.target.setAttribute('width', event.target.width)
+    })
     overlay_title.innerText = title
     overlay_description.innerText = description
     if (price > 0) {
@@ -196,11 +196,12 @@ function populateOverlay(e) {
     }
     overlay_framed.innerText = framed
     overlay_open.setAttribute('href', img)
-    contact_message.innerHTML = "Enquiring about " + title
     overlay_contact.addEventListener('click', () => {
         contact()
+        contact_message.innerHTML = "Enquiring about " + title
     })
 }
 overlay_fit.addEventListener('click', () => {
-    overlay_img.classList.toggle('max-w-[50vw]')
+    overlay_img.classList.toggle('w-full')
+    overlay_img.classList.toggle('max-h-none')
 })
